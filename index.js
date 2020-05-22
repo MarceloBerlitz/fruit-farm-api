0cd330b846abd9f6d719c8cb607aab47ce888e4c
Marcelo Berlitz <marceloberlitz@outlook.com>
2020-05-22 12:10:12 -0300

    Enable cors

--- index.js
+++ index.js
@@ -10,6 +10,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.use(require('./src/app.module')(express));
 
 app.listen(port, () => {
